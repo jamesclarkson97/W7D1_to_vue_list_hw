cbb6784efbe80c57632959eba73ec8f84a7a9020
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-27 16:24:42 +0100

    added new chore to list. MVP done

--- src/app.js
+++ src/app.js
@@ -4,12 +4,20 @@ document.addEventListener('DOMContentLoaded', ()=> {
     new Vue({
         el: "#app",
         data: {
-            items: [
+            chores: [
                 {name: 'Wash car'},
                 {name: 'Cook dinner'},
                 {name: 'Buy umbrella'}
-            ]
+            ],
+            newChore: ''
         },
-        methods: {}
+        methods: {
+            saveNewChore: function() {
+                this.chores.push({
+                    name: this.newChore
+                });
+                this.newChore = '';
+            }
+        }
     });
 });
