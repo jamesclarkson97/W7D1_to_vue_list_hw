62aceca984713d69c18d0d6e1c458d7a64e499b2
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-07-27 15:18:55 +0100

    added list items to app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', ()=> {
     new Vue({
         el: "#app",
-        
+        data: {
+            items: ['Clean car', 'Make dinner', 'Buy umbrella']
+        }
     });
 });
